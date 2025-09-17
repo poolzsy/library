d301e0f9179b91cbafaa082fca911bb8bef0c939
lilac <1773484308@qq.com>
2025-09-17 21:40:54 +0800

    全局异常处理

---
+++ backend/src/main/java/com/lilac/enums/HttpsCodeEnum.java
@@ -0,0 +1,21 @@
+package com.lilac.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum HttpsCodeEnum {
+    SUCCESS(200, "操作成功"),
+    SYSTEM_ERROR(500, "系统内部错误，请联系管理员"),
+
+    BAD_REQUEST(400, "无效的请求参数"),
+    USER_NOT_LOGIN(401, "未授权"),
+    RESOURCE_NOT_FOUND(404, "请求的资源不存在");
+
+    private final Integer code;
+    private final String message;
+
+    HttpsCodeEnum(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
---
+++ backend/src/main/java/com/lilac/exception/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.lilac.exception;
+
+import com.lilac.domain.result.Result;
+import com.lilac.enums.HttpsCodeEnum;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    /**
+     * 处理自定义的业务异常
+     *
+     * @param e 业务异常
+     * @return Result
+     */
+    @ExceptionHandler(SystemException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Result<Void> handleSystemException(SystemException e) {
+        log.warn("业务异常: code={}, message={}", e.getCode(), e.getMessage());
+        return Result.error(e.getCode(), e.getMessage());
+    }
+
+    /**
+     * 处理所有未被捕获的未知异常
+     *
+     * @param e 未知异常
+     * @return Result
+     */
+    @ExceptionHandler(Exception.class)
+    public Result<Void> handleException(Exception e) {
+        log.error("未捕获的系统异常！ ", e);
+        // 对外屏蔽内部细节，返回统一的系统错误提示
+        return Result.error(HttpsCodeEnum.SYSTEM_ERROR);
+    }
+}
