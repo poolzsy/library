fdbea9cc922edd0833eb598e2efcce71cdd4c711
zc <1366232526@qq.com>
2025-10-15 23:00:35 +0800

    角色crud

--- backend/src/main/java/com/lilac/mapper/RoleMapper.java
+++ backend/src/main/java/com/lilac/mapper/RoleMapper.java
@@ -1,6 +1,14 @@
 package com.lilac.mapper;
 
+import com.lilac.annotation.AutoFill;
+import com.lilac.domain.dto.RoleDTO;
+import com.lilac.domain.entity.Role;
+import com.lilac.enums.OperationType;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
 
 /**
  * (Role)表数据库访问层
@@ -10,4 +18,32 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface RoleMapper {
 
+    /**
+     * 通过id查询角色
+     */
+    @Select("select * from role where id = #{id} and del_flag = 0 ")
+    Role SelectById();
+
+    /**
+     * 查询角色
+     */
+    List<Role> page(RoleDTO roleDTO);
+
+    /**
+     * 新增角色
+     */
+    @AutoFill(OperationType.INSERT)
+    void save(Role role);
+
+    /**
+     * 删除用户角色
+     */
+    @Update("update role set del_flag = 1 where id = #{id}")
+    void deleteById(Integer id);
+
+    /**
+     * 修改角色信息
+     */
+    @AutoFill(OperationType.UPDATE)
+    void update(Role role);
 }
--- backend/src/main/java/com/lilac/service/RoleService.java
+++ backend/src/main/java/com/lilac/service/RoleService.java
@@ -1,10 +1,38 @@
 package com.lilac.service;
 
+import com.lilac.domain.dto.AddRoleDTO;
+import com.lilac.domain.dto.RoleDTO;
+import com.lilac.domain.entity.Role;
+import com.lilac.domain.vo.PageVO;
+
 /**
  * (Role)表服务接口
  *
  * @author lilac
  */
 public interface RoleService {
+    /**
+     * 根据id查询角色
+     */
+    Role SelectById(Integer id);
 
+    /**
+     * 查询角色
+     */
+    PageVO page(RoleDTO roleDTO);
+
+    /**
+     * 新增角色
+     */
+    void save(AddRoleDTO addRoleDTO);
+
+    /**
+     * 删除用户角色
+     */
+    void deleteRoleById(Integer id);
+
+    /**
+     * 修改角色信息
+     */
+    void update(Role role);
 }
